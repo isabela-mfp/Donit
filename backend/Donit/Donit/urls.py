171e7451f6bb9a38dfb033116dc4c3de85b81918
BrenoCPimenta <brenocpimenta@gmail.com>
2022-06-25 01:59:16 -0300

    Ajustes na url

--- backend/Donit/Donit/urls.py
+++ backend/Donit/Donit/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     re_path(r'^login$', views.login_function, name='login_function'),
     re_path(r'^register$',  views.registration, name='registration'),
     re_path(r'^logout$',  views.logout_function, name='logout_function'),
-    
+
     re_path(r'^app/new_list$', views.new_list, name='new_list'),
-    re_path(r'^app/del_list$', views.del_list, name='del_list'),
+    re_path(r'^app/del_list/(\d+)$', views.del_list, name='del_list'),
     re_path(r'^app/(\d+)/new_task$', views.new_task, name='new_task'),
     re_path(r'^app/(\d+)/del_task$', views.del_task, name='del_task'),
     path('admin/', admin.site.urls),
