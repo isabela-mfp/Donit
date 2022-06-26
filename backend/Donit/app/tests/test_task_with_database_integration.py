4800a312befa819a8b74cf66fabfe414cdb29a2e
BrenoCPimenta <brenocpimenta@gmail.com>
2022-06-26 19:34:24 -0300

    Add final task integration tasks

--- backend/Donit/app/tests/test_task_with_database_integration.py
+++ backend/Donit/app/tests/test_task_with_database_integration.py
@@ -1,5 +1,6 @@
 from django.test import TransactionTestCase, Client
-from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
 
 
 class TestTaskWithDataBaseIntegration(TransactionTestCase):
@@ -7,7 +8,7 @@ class TestTaskWithDataBaseIntegration(TransactionTestCase):
     # Reset index after each test
     reset_sequences = True
 
-    def test_add_and_retrieve_task_with_anonymous_user(self):
+    def test_add_and_retrieve_task(self):
         c = Client()
         c.post('/app/list',{'name':'ListaTeste001', 'description':'description test example','type':'1'})
         test_task = {
@@ -15,9 +16,49 @@ class TestTaskWithDataBaseIntegration(TransactionTestCase):
             'description': 'description',
             'priority': 1,
             'status': 'T',
-            'dueDate': '2019-10-05',
+            'dueDate': datetime.datetime.now(tz=timezone.utc),
             'list_id': 1
         }
         c.post('/app/task/1/', test_task)
         task_response = c.get('/app/task/1/', HTTP_ACCEPT='application/json')
         self.assertEqual(task_response.json()[0]['fields']['name'], 'test_task given name')
+
+    def test_add_and_delte(self):
+        c = Client()
+        c.post('/app/list',{'name':'ListaTeste001', 'description':'description test example','type':'1'})
+        test_task = {
+            'name': 'test_task given name',
+            'description': 'description',
+            'priority': 1,
+            'status': 'T',
+            'dueDate': datetime.datetime.now(tz=timezone.utc),
+            'list_id': 1
+        }
+        c.post('/app/task/1/', test_task)
+        c.delete('/app/task/1/')
+        task_response = c.get('/app/task/1/', HTTP_ACCEPT='application/json')
+        self.assertEqual(task_response.json(), [])
+
+    def test_add_two_tasks(self):
+        c = Client()
+        c.post('/app/list',{'name':'ListaTeste001', 'description':'description test example','type':'1'})
+        test_task = {
+            'name': 'test_task given name',
+            'description': 'description',
+            'priority': 1,
+            'status': 'T',
+            'dueDate': datetime.datetime.now(tz=timezone.utc),
+            'list_id': 1
+        }
+        test_task_2 = {
+            'name': 'second test_task given name',
+            'description': 'description',
+            'priority': 1,
+            'status': 'T',
+            'dueDate': datetime.datetime.now(tz=timezone.utc),
+            'list_id': 1
+        }
+        c.post('/app/task/1/', test_task)
+        c.post('/app/task/1/', test_task_2)
+        task_response = c.get('/app/task/1/', HTTP_ACCEPT='application/json')
+        self.assertEqual(task_response.json()[1]['fields']['name'], 'second test_task given name')
