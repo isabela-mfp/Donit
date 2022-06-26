18778b543819baea4553c0b5870b3d160b7d5dbc
BrenoCPimenta <brenocpimenta@gmail.com>
2022-06-26 18:34:06 -0300

    Add list integration dabase test for auth user

--- backend/Donit/app/tests/test_integration.py
+++ backend/Donit/app/tests/test_integration.py
@@ -1,4 +1,5 @@
 from django.test import TransactionTestCase, Client
+from django.contrib.auth.models import User
 
 
 class TestDataBaseIntegration(TransactionTestCase):
@@ -19,3 +20,13 @@ class TestDataBaseIntegration(TransactionTestCase):
         c.delete('/app/list/1/')
         lists_response = c.get('/app/list', HTTP_ACCEPT='application/json')
         self.assertEqual(lists_response.json(), [])
+
+    def test_add_and_retrieve_list_with_auth_user(self):
+        User.objects.create_user(username='dummy1', password='dummypass1')
+        User.objects.create_user(username='dummy2', password='dummypass2')
+        c = Client()
+        c.post('/login', {'username':'dummy2', 'password':'dummypass2'})
+        c.post('/app/list',{'name':'ListaTeste001', 'description':'description test example','type':'1'})
+        lists_response = c.get('/app/list', HTTP_ACCEPT='application/json')
+        self.assertEqual(lists_response.json()[0]['fields']['name'], 'ListaTeste001')
+        self.assertEqual(lists_response.json()[0]['fields']['userid'], 2)
