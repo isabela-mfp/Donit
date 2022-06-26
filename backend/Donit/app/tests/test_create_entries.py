07e6621a5458893b2de0d4faba1ced922425c4d5
lcharles123 <tidus098@hotmail.com>
2022-06-26 18:14:48 +0100

    corrigindo alguns testes

--- backend/Donit/app/tests/test_create_entries.py
+++ backend/Donit/app/tests/test_create_entries.py
@@ -31,26 +31,24 @@ class TestCreateListsOnDatabase(TestCase):
 
 class TestCreateTasksOnDatabase(TestCase):
     def setUp(self):
-        self.u1 = User(username='dummy')
-        self.u1.save() # User model provided by django
-        self.T1 = TaskManagement(name='Tarefa 1', description='Esta eh a primeira tarefa de teste', conclusion=datetime.date(2022, 9, 9), priority=1, status='T')
-        self.T2 = TaskManagement(name='Tarefa 2', description='Exemplo de tarefa do supermercado', conclusion=datetime.date(2022, 9, 9), priority=1, status='T')
-        self.T1.userid = self.u1
-        self.T2.userid = self.u1
-        self.T1.save()
-        #self.T2.save()
+        self.user1 = User(username='dummy')
+        self.user1.save()
+        self.test_list = ListManagement(name='Lista 1', userid=self.user1, description='Esta eh a primeira lista de teste', type='A')
+        self.test_list.save()
+        self.task1 = TaskManagement(name='Tarefa 1', listid=self.test_list, description='Esta eh a primeira tarefa de teste', conclusion=datetime.date(2022, 9, 9), priority=1, status='T')
+        self.task2 = TaskManagement(name='Tarefa 2', listid=self.test_list, description='Exemplo de tarefa do supermercado', conclusion=datetime.date(2022, 9, 9), priority=1, status='T')
+        self.task1.save()
+        self.task2.save()   
     
     def test_create_2_tasks_and_save_to_database(self): 
-        pass
-        #self.assertEqual(TaskManagement.objects.all().count(), 2)
+        self.assertEqual(TaskManagement.objects.all().count(), 2)
     
     def test_get_and_compare_tasks_on_database(self):
-        return
         saved_items = TaskManagement.objects.all()
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.name, 'Tarefa 1')
-        self.assertEqual(first_saved_item, self.T1)
+        self.assertEqual(first_saved_item, self.task1)
         self.assertEqual(second_saved_item.name, 'Tarefa 2')
-        self.assertEqual(second_saved_item, self.T2)
+        self.assertEqual(second_saved_item, self.task2)
 
--- backend/Donit/app/tests/test_delete_entries.py
+++ backend/Donit/app/tests/test_delete_entries.py
@@ -8,18 +8,19 @@ class TestDeleteEntries(TestCase):
     def setUp(self):
         self.user1 = User(1)
         self.user1.save()
-        self.dummy_list = ListManagement(userid=self.user1, name="lista 1", type="N", description="esta eh uma lista 1")
     
     def test_criar_lista_e_deletar(self): 
-        test_list = ListManagement(name='Lista 1', userid_id=1, description='Esta eh a primeira lista de teste', type='A')
+        test_list = ListManagement(name='Lista 1', userid=self.user1, description='Esta eh a primeira lista de teste', type='A')
         test_list.save()
         test_list.delete()
         self.assertEqual(ListManagement.objects.all().count(), 0)
     
     def test_criar_tarefa_e_deletar(self): 
-        test_task = TaskManagement(name='Tarefa 1', listid=self.dummy_list
+        dummy_list = ListManagement(userid=self.user1, name="lista 1", type="N", description="esta eh uma lista 1")
+        dummy_list.save()
+        test_task = TaskManagement(name='Tarefa 1', listid=dummy_list
             , description='Esta eh a primeira tarefa de teste', conclusion=datetime.date(2022, 9, 9), priority=1, status='T')
         test_task.save()
-        test_task.objects.filter(id=test_task.id).delete()
-        self.assertEqual(ListManagement.objects.all().count(), 0)
+        TaskManagement.objects.filter(id=test_task.id).delete()
+        self.assertEqual(TaskManagement.objects.all().count(), 0)
 
