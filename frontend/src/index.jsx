b319606a97d375761f77cd841219ce8604ffea9b
Rafael Gonçalves <rafael-goncalves-oliveira@outlook.com>
2022-06-25 23:20:42 -0300

    Change to localhost

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000';
+axios.defaults.baseURL = 'http://localhost:8000';
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 axios.defaults.withCredentials = true;
